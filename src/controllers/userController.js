5639451b3253281065fee6cf007bc6ec14c55a0e
Alex Piter <alexsoares2517@gmail.com>
2022-10-17 20:03:24 -0300

    adiciona função de autenticação

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,57 @@
 const userService = require("../services/userService.js");
+
+const express = require('express')
+const app = express();
+const expressBasicAuth = require('express-basic-auth')
+
+app.use(expressBasicAuth({authorizer: customAuthorizer}));
+
+
+function customAuthorizer(username, password, db_username, db_password) {
+  const userMatches = expressBasicAuth.safeCompare(username.toString(), db_username)
+  const passwordMatches = expressBasicAuth.safeCompare(password.toString(), db_password)
+  
+  return userMatches & passwordMatches
+}
+
 module.exports = userController = {
+  basicAuth: async(req, res, next) => {
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(403).send('É necessário estar logado.');
+      }
+      
+      const encoded = authHeader.substring(6);
+      const decoded = Buffer.from(encoded.toString(), 'base64').toString('ascii');
+      
+      try {
+        const [username, password] = decoded.split(':');
+        const dbUserInfo = await userService.getByUsername(username);
+        const dbUsername = dbUserInfo[0].username;
+        const dbPassword = dbUserInfo[0].password;
+        
+        const isAuthorized = customAuthorizer(username, password, dbUsername, dbPassword)
+        
+        if (!isAuthorized) {
+          return res.status(403).send('Usuário ou senha incorreta.');
+        }
+        
+        if (req.header('username') !== dbUsername) {
+          return res.status(403).send('Usuário enviado pelo header não autorizado!');
+        }
+      } catch (error) {
+        if (res.status(403)){
+          return res.status(403).send('Usuário ou senha incorreta.');
+        }
+      }
+      
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  
   getAll: async (req, res, next) => {
     try {
       const users = await userService.getAll();
@@ -11,7 +63,7 @@ module.exports = userController = {
   
   getById: async (req, res, next) => {
     try {
-      const user = await userService.getById(req.params.id);
+      const user = await userService.getByUsername(req.params.id);
       res.json(user);
     } catch (error) {
       next(error);
