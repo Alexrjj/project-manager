65590abb73cc91d053838aa7c85dfc0944b336d9
Alex Piter <alexsoares2517@gmail.com>
2022-10-19 19:48:01 -0300

    adiciona e configura moment, ejs, body-parser

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
-const express  = require('express')
+const express = require('express')
 const app = express();
-app.use(express.json());
+const bodyParser = require('body-parser');
+const moment = require('moment');
 
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Static Files
+app.use(express.static('public'));
+
+// app.use(express.json());
+
+// Routes
 const userRouter = require('./routes/user.js');
 const projectRouter = require('./routes/project.js');
-
 app.use('/users', userRouter);
 app.use(["/project", "/projects"], projectRouter);
 
+// Express server
 app.listen(3000, () => {
   console.log("Server running.");
 });
