8380bdf103ded83fbd0222eb9426e5560f989734
Alex Piter <alexsoares2517@gmail.com>
2022-10-20 20:58:23 -0300

    adiciona rotas registerUser (get) e saveUser (post)

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,13 +3,24 @@ const router = express.Router();
 const userController = require("../controllers/userController.js");
 
 /* GET users listing. */
+
+
 router.route("/")
   .get(userController.getAll)
   .post(userController.create);
+
+router.route('/registerUser')
+  .get(userController.registerUser);
+
+router.route('/saveUser')
+  .post(userController.create);
+
+router.route('/delete')
+  .post(userController.delete);
+module.exports = router;
+
 router.route("/:id")
   .get(userController.getById)
   .put(userController.update)
 
-router.route('/delete')
-  .post(userController.delete);
 module.exports = router;
