b400a567eaabd19c30d8b42c57834f15d404c126
Alex Piter <alexsoares2517@gmail.com>
2022-10-21 16:25:33 -0300

    altera variaveis de conexão

--- knexfile.js
+++ knexfile.js
@@ -10,8 +10,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: process.env.POSTGRES_DATABASE,
-      user: process.env.POSTGRES_HOST,
-      password: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
     }
   },
   
