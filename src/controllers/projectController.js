2c9fbf4e203b0cc400b8d849050db3e2eba05b2d
Alex Piter <alexsoares2517@gmail.com>
2022-10-25 23:11:37 -0300

    fixa bugs nas rotas de criação, deleção e listagem de projetos

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,7 @@
 const projectService = require("../services/projectService.js");
-const express = require('express');
-const app = express();
+const moment = require('moment');
+const userService = require("../services/userService");
+// const session = require('express-session');
 
 module.exports = projectController = {
   getById: async (req, res, next) => {
@@ -14,9 +15,23 @@ module.exports = projectController = {
   
   getByUser: async (req, res, next) => {
     try {
-      const username = await req.app.locals;
-      const project = await projectService.getByUser(username);
-      res.json(project);
+      const username = await req.session.username;
+      const projects = await projectService.getByUser(username);
+      // res.json(project);
+      
+      res.render('projects', {
+        projects: projects,
+        moment: moment
+      })
+    } catch (error) {
+      // next(error);
+      res.send('<h1>Você precisa estar logado!</h1>')
+    }
+  },
+  
+  registerProject: async (req, res, next) => {
+    try {
+      res.render('registerProject');
     } catch (error) {
       next(error);
     }
@@ -24,9 +39,26 @@ module.exports = projectController = {
   
   create: async (req, res, next) => {
     try {
-      const username = req.header('username');
-      const project = await projectService.create(username, req.body);
-      res.json(project);
+      const username = req.session.username;
+  
+      let titulo = req.body.titulo;
+      let cep = req.body.cep;
+      let custo = req.body.custo;
+      let prazo = req.body.prazo;
+      
+      if (titulo !== undefined && cep !== undefined && custo !== undefined && prazo !== undefined) {
+        const checkProject = await projectService.projectExists(username, titulo)
+        if (checkProject.toString() === "") {
+          await projectService.create(username, titulo, cep, custo, prazo);
+          res.redirect('/projects');
+        } else {
+          res.send('Projeto existente.');
+          // res.redirect('/users/registerUser');
+        }
+      }
+  
+      // res.json(user);
+      
     } catch (error) {
       next(error);
     }
@@ -53,12 +85,9 @@ module.exports = projectController = {
   
   delete: async (req, res, next) => {
     try {
-      const project = await projectService.delete(req.header('username'), req.params.id);
-      if (project === 1) {
-        res.send(`Projeto ${req.params.id} deletado.`);
-      } else {
-        res.send(`Projeto não encontrado.`);
-      }
+      let idProject = req.body.idProject;
+      await projectService.delete(req.session.username, idProject);
+      res.redirect('/projects');
       // res.json(project);
       
     } catch (error) {
